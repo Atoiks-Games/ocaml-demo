d01ccd3e69166115ea91919905217510392ec617
Paul Teng <plankp@outlook.com>
2019-02-20 08:30:56 -0500

    No more blocky movement

--- main.ml
+++ main.ml
@@ -4,46 +4,77 @@ open Result
 type game_state =
     | Next of {
         player_x : int;
-        player_y : int; }
+        player_y : int;
+        has_key_up : bool;
+        has_key_down : bool;
+        has_key_left : bool;
+        has_key_right : bool; }
     | Halt
 ;;
 
 let make_initial_state () =
     Next {
         player_x = 0;
-        player_y = 0; }
+        player_y = 0;
+        has_key_up = false;
+        has_key_down = false;
+        has_key_left = false;
+        has_key_right = false; }
 ;;
 
+let is_up_key kc = kc = Sdl.K.w || kc = Sdl.K.up;;
+let is_down_key kc = kc = Sdl.K.s || kc = Sdl.K.down;;
+let is_left_key kc = kc = Sdl.K.a || kc = Sdl.K.left;;
+let is_right_key kc = kc = Sdl.K.d || kc = Sdl.K.right;;
+
 (* return Halt if you want to stop the game *)
-let my_update gstate =
-    let evt = Sdl.Event.create () in
-    if not (Sdl.poll_event (Some evt)) then
-        gstate
-    else begin
-        let evt_type = Sdl.Event.get evt Sdl.Event.typ in
-        if evt_type = Sdl.Event.quit then
-            Halt
-        else if evt_type = Sdl.Event.key_down then
-            (* don't care if the key is repeating *)
-            let kc = Sdl.Event.get evt Sdl.Event.keyboard_keycode in
-            let dy =
-                let dw = if kc = Sdl.K.w || kc = Sdl.K.up then 1 else 0
-                and ds = if kc = Sdl.K.s || kc = Sdl.K.down then -1 else 0 in
-                dw + ds
-            and dx =
-                let dd = if kc = Sdl.K.d || kc = Sdl.K.right then 1 else 0
-                and da = if kc = Sdl.K.a || kc = Sdl.K.left then -1 else 0 in
-                da + dd in
-            match gstate with
-            | Next {player_x = x; player_y = y} -> Next {
-                player_x = x + dx;
-                player_y = y + dy; }
-            | Halt -> Halt
-        else
-            (* you would handle some more events
-             * (such as mouse clicks and stuff)
-             * here! *)
-            gstate
+let my_update = function
+    | Halt -> Halt  (* should not happen *)
+    | Next state_data -> begin
+        let evt = Sdl.Event.create () in
+        if not (Sdl.poll_event (Some evt)) then
+            let has_key_up = state_data.has_key_up
+            and has_key_down = state_data.has_key_down
+            and has_key_left = state_data.has_key_left
+            and has_key_right = state_data.has_key_right in
+            let dx = (if state_data.has_key_right then 1 else 0) + (if state_data.has_key_left then -1 else 0)
+            and dy = (if state_data.has_key_down then 1 else 0) + (if state_data.has_key_up then -1 else 0) in
+            Next {
+                player_x = state_data.player_x + dx;
+                player_y = state_data.player_y + dy;
+                has_key_up; has_key_down; has_key_left; has_key_right }
+        else begin
+            let evt_type = Sdl.Event.get evt Sdl.Event.typ in
+            if evt_type = Sdl.Event.quit then
+                Halt
+            else if evt_type = Sdl.Event.key_down then
+                (* don't care if the key is repeating *)
+                let kc = Sdl.Event.get evt Sdl.Event.keyboard_keycode in
+                let has_key_up = state_data.has_key_up || is_up_key kc
+                and has_key_down = state_data.has_key_down || is_down_key kc
+                and has_key_left = state_data.has_key_left || is_left_key kc
+                and has_key_right = state_data.has_key_right || is_right_key kc in
+                Next {
+                    player_x = state_data.player_x;
+                    player_y = state_data.player_y;
+                    has_key_up; has_key_down; has_key_left; has_key_right }
+            else if evt_type = Sdl.Event.key_up then
+                (* don't care if the key is repeating *)
+                let kc = Sdl.Event.get evt Sdl.Event.keyboard_keycode in
+                let has_key_up = not (not state_data.has_key_up || is_up_key kc)
+                and has_key_down = not (not state_data.has_key_down || is_down_key kc)
+                and has_key_left = not (not state_data.has_key_left || is_left_key kc)
+                and has_key_right = not (not state_data.has_key_right || is_right_key kc) in
+                Next {
+                    player_x = state_data.player_x;
+                    player_y = state_data.player_y;
+                    has_key_up; has_key_down; has_key_left; has_key_right }
+            else
+                (* you would handle some more events
+                * (such as mouse clicks and stuff)
+                * here! *)
+                Next state_data
+        end
     end
 ;;
 
