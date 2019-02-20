999c7671d7c1a6e6b772514ff855e2193450495b
Paul Teng <plankp@outlook.com>
2019-02-20 08:05:41 -0500

    Add keyboard and states

--- main.ml
+++ main.ml
@@ -1,41 +1,79 @@
 open Tsdl
 open Result
 
-(* return false if you want to stop the game *)
-let my_update () =
+type game_state =
+    | Next of {
+        player_x : int;
+        player_y : int; }
+    | Halt
+;;
+
+let make_initial_state () =
+    Next {
+        player_x = 0;
+        player_y = 0; }
+;;
+
+(* return Halt if you want to stop the game *)
+let my_update gstate =
     let evt = Sdl.Event.create () in
     if not (Sdl.poll_event (Some evt)) then
-        true
+        gstate
     else begin
         let evt_type = Sdl.Event.get evt Sdl.Event.typ in
         if evt_type = Sdl.Event.quit then
-            false
+            Halt
+        else if evt_type = Sdl.Event.key_down then
+            (* don't care if the key is repeating *)
+            let kc = Sdl.Event.get evt Sdl.Event.keyboard_keycode in
+            let dy =
+                let dw = if kc = Sdl.K.w || kc = Sdl.K.up then 1 else 0
+                and ds = if kc = Sdl.K.s || kc = Sdl.K.down then -1 else 0 in
+                dw + ds
+            and dx =
+                let dd = if kc = Sdl.K.d || kc = Sdl.K.right then 1 else 0
+                and da = if kc = Sdl.K.a || kc = Sdl.K.left then -1 else 0 in
+                da + dd in
+            match gstate with
+            | Next {player_x = x; player_y = y} -> Next {
+                player_x = x + dx;
+                player_y = y + dy; }
+            | Halt -> Halt
         else
             (* you would handle some more events
              * (such as mouse clicks and stuff)
              * here! *)
-            true
+            gstate
     end
 ;;
 
 (* renders stuff *)
-let my_render renderer =
-    Sdl.set_render_draw_color renderer 0xFF 0xFF 0xFF 0xFF |> ignore;
-    Sdl.render_clear renderer |> ignore;
+let my_render renderer = function
+    | Halt -> () (* this should not happen, oh well... *)
+    | Next state_data -> begin
+        Sdl.set_render_draw_color renderer 0xFF 0xFF 0xFF 0xFF |> ignore;
+        Sdl.render_clear renderer |> ignore;
 
-    Sdl.set_render_draw_color renderer 0xFF 0x00 0x00 0xFF |> ignore;
-    Sdl.render_fill_rect renderer (Some (Sdl.Rect.create 10 10 20 20)) |> ignore;
+        (* keeping the *terrible* gfx tradition, out player will just be a red box (for now) *)
+        Sdl.set_render_draw_color renderer 0xFF 0x00 0x00 0xFF |> ignore;
+        Sdl.render_fill_rect renderer (Some (Sdl.Rect.create state_data.player_x state_data.player_y 20 20)) |> ignore;
+    end
 ;;
 
 (* this does my_update -> my_render -> blit to renderer -> ... *)
-let rec game_loop renderer =
-    if not (my_update ()) then
-        ()
-    else begin
-        my_render renderer;
-        Sdl.render_present renderer;
-        game_loop renderer;
-    end
+let game_loop renderer =
+    let rec inner_loop state =
+        let next_state = my_update state in
+        match next_state with
+        | Halt -> ()
+        | Next datum -> begin
+            (* only do rendering and continue loop if we are continuing *)
+            my_render renderer (Next datum);
+            Sdl.render_present renderer;
+            inner_loop next_state;
+        end in
+    (* start the inner loop with inital game state *)
+    inner_loop (make_initial_state ())
 ;;
 
 let main () = match Sdl.init Sdl.Init.video with
